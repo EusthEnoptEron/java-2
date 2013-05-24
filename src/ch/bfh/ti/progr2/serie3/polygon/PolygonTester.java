6fdfaa8394c45d4fcd82f0492eb913ad7403f0d0
Simon Meer <simon.meer@bluewin.ch>
2013-05-24 08:21:08 +0200

    Add more tests

--- src/ch/bfh/ti/progr2/serie3/polygon/PolygonTester.java
+++ src/ch/bfh/ti/progr2/serie3/polygon/PolygonTester.java
@@ -13,12 +13,28 @@ public class PolygonTester {
 
 		// Make a 4x4 rect
 		Polygon rect = new Polygon(
-			new int[]{0, 4, 4, 0},
-			new int[]{0, 0, 4, 4},
+			new int[] {0, 4, 4, 0},
+			new int[] {0, 0, 3, 3},
 		4);
 
-		printTest(16.0, rect.getArea());
+		Polygon parallelogram = new Polygon(
+			new int[] {1, 5, 4, 0},
+			new int[] {0, 0, 3, 3},
+		4);
 
+		Polygon trapezoid = new Polygon(
+			new int[] {3, 5, 7, 0},
+			new int[] {4, 4, 0, 0},
+		4);
+
+		printTest(12.0, rect.getArea());
+		printTest(12.0, parallelogram.getArea());
+
+		// h = 4
+		// b1 = 5 - 3 = 2
+		// b2 = 7 - 0 = 7
+		// A = h/2*(b1+b2)
+		printTest(4/2.0 * (2+7), trapezoid.getArea());
 	}
 	private static void printTest(double expected, double area) {
 		System.out.printf("Expected: %.2f, got: %.2f\n", expected, area);
